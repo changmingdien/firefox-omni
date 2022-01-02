44117e606ed451182a1149a84ef026e8b4e7ef1a
Morgan Chang <mingdien.chang@gmail.com>
2022-01-02 16:37:42 +0800

    copy omni.ja back to the source path

--- key-privatebrowsing-windows.sh
+++ key-privatebrowsing-windows.sh
@@ -8,6 +8,9 @@ unzip -d omni omni.ja
 sed -i -e '/<key id="key_privatebrowsing"/{ N; s/accel,shift/accel,alt,shift/}' \
 	omni/chrome/browser/content/browser/browser.xhtml
 cd omni && /c/Program\ Files/7-Zip/7z.exe a ../omni.ja.zip *
+rm -f /c/Program\ Files/Mozilla\ Firefox/browser/omni.ja
+cp omni.ja.zip /c/Program\ Files/Mozilla\ Firefox/browser/omni.ja
 
 
-rm /c/Users/morgan.chang/AppData/Local/Mozilla/Firefox/Profiles/810fjj3t.default/startupCache/startupCache.8.little
+rm -f /c/Users/morgan.chang/AppData/Local/Mozilla/Firefox/Profiles/810fjj3t.default/startupCache/startupCache.8.little
+rm -f /c/Users/morgan/AppData/Local/Mozilla/Firefox/Profiles/ge0idutt.default-beta/startupCache/startupCache.8.little
