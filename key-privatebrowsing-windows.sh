a261bbe4afe4308936aa8168c3bebbec3aa5761e
Chang, Ming-Dien <mingdien.chang@gmail.com>
2022-02-10 15:21:59 +0800

    merge Linux and Windwos versions
    注意 $OMNI 必須被 double-quoted，才能保留 Windows 路徑中的空白。
    $ZIP 同理，但其參數不能擺在一起，因為 double-quoted後似乎會被認為都是路徑
    的一部份，而被 bash 認為檔案不存在！

--- key-privatebrowsing-windows.sh
+++ key-privatebrowsing-windows.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
 rm -rf omni *.zip
-rm -rf *.zip
 mkdir -p omni
-cp /c/Program\ Files/Mozilla\ Firefox/browser/omni.ja ./
+
+[ -e /c/Program\ Files/Mozilla\ Firefox/browser/omni.ja ] && OMNI=/c/Program\ Files/Mozilla\ Firefox/browser/omni.ja
+[ -e /usr/lib/firefox/browser/omni.ja ] && OMNI=/usr/lib/firefox/browser/omni.ja
+cp "$OMNI" ./
+
 unzip -d omni omni.ja
 sed -i -e '/<key id="key_privatebrowsing"/{ N; s/accel,shift/accel,alt,shift/}' \
 	omni/chrome/browser/content/browser/browser.xhtml
-cd omni && /c/Program\ Files/7-Zip/7z.exe a ../omni.ja.zip *
+
+which zip &> /dev/null && ZIP=zip OPT="-r" || ZIP=/c/Program\ Files/7-Zip/7z.exe OPT='a'
+cd omni && "$ZIP" $OPT ../omni.ja.zip *
 cd ..
-rm -f /c/Program\ Files/Mozilla\ Firefox/browser/omni.ja
-cp omni.ja.zip /c/Program\ Files/Mozilla\ Firefox/browser/omni.ja
+rm -f "$OMNI"
+cp omni.ja.zip "$OMNI"
 
-
+# Linux aries
+rm  /home/morgan/.cache/mozilla/firefox/47f7vds8.default/startupCache/startupCache.8.little
+# Windows 7 VATICS
 rm -f /c/Users/morgan.chang/AppData/Local/Mozilla/Firefox/Profiles/810fjj3t.default/startupCache/startupCache.8.little
+# Windows 10 orion
 rm -f /c/Users/morgan/AppData/Local/Mozilla/Firefox/Profiles/ge0idutt.default-beta/startupCache/startupCache.8.little
